6f0f1413c4132e31eec6f96c4dd8df8ea6228f0e
Abhishek <72578187+abhish127@users.noreply.github.com>
2022-05-24 22:59:24 -0700

    Update Differentiate.java

--- Differentiate.java
+++ Differentiate.java
@@ -3,9 +3,6 @@ import javax.swing.tree.TreeModel;
 
 /**
  * Performs differentiating calculations.
- * Time to implement networking.
- * 
- * @version 5/18/22 - 11:50 pm
  */
 public class Differentiate {
 
@@ -84,23 +81,15 @@ public class Differentiate {
                     int index = termOrSign.indexOf("^");
                     String coefficient = termOrSign.substring(0, index - 1);
 
-                    // // System.out.println(termOrSign.substring(index - 1, index + 2));
-
                     String derivativeTerm = derivatives.get(termOrSign.substring(index - 1, termOrSign.length()));
-
-                    // System.out.print("Derivative term: " + derivativeTerm);
 
                     int preCoefficient = 1;
 
-                    // 5x^3 => derivative term = 3x^2 and coefficiient is 5
-                    // 3x^2 => currentCoefficient = 3
                     if (termOrSign.indexOf("^") - 2 >= 0) {
                         String numCoefficient = termOrSign.substring(0, termOrSign.indexOf("^") - 1);
                         preCoefficient = Integer.parseInt(numCoefficient); // stores the coefficient
                     }
 
-                    // // System.out.println("this is the pre coefficient : " + preCoefficient);
-                    // // System.out.println("this is the derivative term: " + derivativeTerm);
                     int derivCoefficient = 1;
                     if (!derivativeTerm.substring(0, 1).equals("x")) {
                         derivCoefficient = Integer.parseInt(derivativeTerm.substring(0, 1));
@@ -108,11 +97,7 @@ public class Differentiate {
 
                     String finalCoefficient = (derivCoefficient * preCoefficient) + "";
                     String finalExpression = finalCoefficient + derivativeTerm.substring(1);
-                    // // System.out.println(finalCoefficient);
-                    // // System.out.println(finalExpression);
                     answer += finalExpression + " ";
-
-                    // // System.out.println("weee " + answer);
                 }
 
                 // if it's cos(x) or sin(x)
@@ -127,11 +112,7 @@ public class Differentiate {
                      * fullDerivTerm = derivCoeff + derivTerm;
                      */
                     if (answer.length() > 0) {
-                        // // System.out.println("AAANNNSSS: " + answer + "/");
-                        // // System.out.println("correctline?");
                         char sign = answer.charAt(answer.length() - 2);
-                        // // System.out.println("correctline?");
-                        // // System.out.println("sign: " + sign);
                         if (sign == '-' && derivTerm.charAt(0) == '-') {
 
                             answer = answer.substring(0, answer.length() - 2) + "+ " + derivTerm.substring(2) + " ";
@@ -143,20 +124,15 @@ public class Differentiate {
                             answer += derivTerm + " ";
                         }
                     } else {
-                        // // System.out.println("sinnnn to cosss?: " + derivTerm);
                         answer += derivTerm + " ";
-
                     }
-                    // // System.out.println("yo" + answer);
                 }
             }
 
             else {
 
                 answer += termOrSign + " ";
-                // // System.out.println("dadada " + answer);
             }
-
         }
 
         return answer;
@@ -229,28 +205,16 @@ public class Differentiate {
 
     public static void main(String[] args) {
         Differentiate diff = new Differentiate();
-        // String answer = diff.differentiateString("x + sin(x)");
-        // // System.out.println("ans: " + answer);
 
         FunctionsList fl = new FunctionsList("functions.txt");
         while (fl.hasQuestions()) {
             int i = 0;
             String function = fl.nextFunction();
             while (i < 10) {
-                // function = diff.differentiateString(function);
-                // // System.out.println(function);
-                // System.out.println(formatSubscript(function, true));
                 i++;
                 break;
             }
         }
-
-        // added in parameters just so that the program would work; feel free to change
-        // it
-        // System.out.println(formatSubscript("3x^2 + 5x^3 + 6x^2 + cos(x)", true));
-        // System.out.println(formatSubscript("x + cos(x)", false));
-
-        // // System.out.println(diff.differentiateString("69"));
     }
 
 }
